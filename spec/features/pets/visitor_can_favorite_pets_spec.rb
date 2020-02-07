6349dcf298c25e366b7c58c7cd8e9bb9f350cd0e
Cassie Achzenick <caachzenick@gmail.com>
2020-02-07 16:07:00 -0700

    Add user story 16 feature tests

--- spec/features/pets/visitor_can_favorite_pets_spec.rb
+++ spec/features/pets/visitor_can_favorite_pets_spec.rb
@@ -15,11 +15,17 @@ RSpec.describe "Visitor can favorite and view their favorites" do
   it "Can add pets to favorite list" do
     shelter1 = Shelter.create!(name: 'humane society', address: "1234 st", city: 'Denver', state: 'Colorado', zip: "29572")
     snickers = Pet.create!(image: 'https://images-na.ssl-images-amazon.com/images/I/41Q-6cQEOLL._AC_SY400_.jpg', name: 'Snickers', age: 15, sex: 'Female', shelter: shelter1)
+
     visit "/pets/#{snickers.id}"
+
     expect(page).to have_link("Favorite")
+
     expect(page).to have_content("Favorites: 0")
+
     click_link("Favorite")
+
     expect(current_path).to eq("/pets/#{snickers.id}")
+
     expect(page).to have_content("Favorites: 1")
     expect(page).to have_content("#{snickers.name} has been added to your favorites")
   end
