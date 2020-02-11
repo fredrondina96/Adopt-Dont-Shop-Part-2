a285a8a9dbbac8a8190e966eee56e87bb8700e62
Cassie Achzenick <caachzenick@gmail.com>
2020-02-11 09:51:26 -0700

    Add user story 23 test

--- spec/features/applications/visitor_can_approve_applications_spec.rb
+++ spec/features/applications/visitor_can_approve_applications_spec.rb
@@ -64,4 +64,41 @@ RSpec.describe "Visitors can approve applications" do
     expect(page).to have_content("Status: Pending")
     expect(page).to have_content("On hold for Chelsea")
   end
+
+  it "only 1 application can be approved per pet" do
+    shelter1 = Shelter.create!(name: 'humane society', address: "1234 st", city: 'Denver', state: 'Colorado', zip: "29572")
+
+    snickers = Pet.create!(image: 'https://images-na.ssl-images-amazon.com/images/I/41Q-6cQEOLL._AC_SY400_.jpg', name: 'Snickers', age: 15, sex: 'Female', shelter: shelter1, adoption_statu: "pending")
+    sadie = Pet.create!(image: 'https://images.halloweencostumes.com/products/45834/1-1/dog-dino-pup-costume.jpg', name: 'Sadie', age: 3, sex: "Male", shelter: shelter1, adoption_statu: "pending")
+
+    application1 = Application.create!(name: 'Chelsea', address: 'something st', city: "Denver", state: "CO", zip: "39403", phone_number: "5739495835", description: "because", status: "approved")
+    application2 = Application.create!(name: 'Cassie', address: 'nothing st', city: "Seattle", state: "WA", zip: "39402", phone_number: "7777777777", description: "I love dogs")
+
+    application1.pets << snickers
+    application1.pets << sadie
+    application2.pets << snickers
+    application2.pets << sadie
+
+    visit "/pets/#{sadie.id}"
+
+    expect(page).to have_content("Status: Pending")
+    expect(page).to have_content("On hold for Chelsea")
+
+    visit "/pets/#{snickers.id}"
+
+    expect(page).to have_content("Status: Pending")
+    expect(page).to have_content("On hold for Chelsea")
+
+    visit "/applications/#{application2.id}"
+
+    within("#application-#{sadie.id}") do
+      page.check
+    end
+
+    click_button("Approve All Selected Pets")
+
+    expect(current_path).to eq("/applications/#{application2.id}")
+
+    expect(page).to have_content("No more applications can be approved for this pet at this time.")
+  end
 end
