f4c91c652108e0e8c465970243d8f6ca11507a49
Cassie Achzenick <caachzenick@gmail.com>
2020-02-11 18:34:35 -0700

    Add user story 30 test

--- spec/features/shelters/visitor_sees_specific_information_for_specific_shleter_spec.rb
+++ spec/features/shelters/visitor_sees_specific_information_for_specific_shleter_spec.rb
@@ -30,4 +30,28 @@ RSpec.describe "Shelter ID displays all shelter info", type: :feature do
     expect(page).to have_content(shleter3.state)
     expect(page).to have_content(shleter3.zip)
   end
+
+  it "shows statistics for each shelter on its show page" do
+    shelter1 = Shelter.create!(name: 'humane society', address: "1234 st", city: 'Denver', state: 'Colorado', zip: "29572")
+
+    review1 = Review.create!(title: "Best Shelter EVER!", rating: "5", content: "I now have my best friend because of this place", shelter_id: shelter1)
+    review2 = Review.create!(title: "Above Average Shelter", rating: "4", content: "I am pretty sure they gave me a dinosaur not a dog.", picture: "https://s7d2.scene7.com/is/image/PetSmart/5290431", shelter_id: shelter1)
+
+    snickers = Pet.create!(image: 'https://images-na.ssl-images-amazon.com/images/I/41Q-6cQEOLL._AC_SY400_.jpg', name: 'Snickers', age: 15, sex: 'Female', shelter: shelter1)
+    sadie = Pet.create!(image: 'https://images.halloweencostumes.com/products/45834/1-1/dog-dino-pup-costume.jpg', name: 'Sadie', age: 3, sex: "Male", shelter: shelter1)
+    abbey = Pet.create!(image: 'https://petcostumecenter.com/wp-content/uploads/2019/08/Pupasaurus-Rex-T-Rex-Dog-Cat-Costume-.png', name: 'Abbey', age: 6, sex: "Male", shelter: shelter1)
+
+    application1 = Application.create!(name: 'Chelsea', address: 'something st', city: "Denver", state: "CO", zip: "39403", phone_number: "5739495835", description: "because")
+    application2 = Application.create!(name: 'Cassie', address: 'nothing st', city: "Seattle", state: "WA", zip: "39402", phone_number: "7777777777", description: "I love dogs")
+
+    application1.pets << snickers
+    application1.pets << sadie
+    application2.pets << abbey
+
+    visit "/shelters/#{shelter1.id}"
+
+    expect(page).to have_content("Total pets: 3")
+    expect(page).to have_content("Average Review Rating: 4.5")
+    expect(page).to have_content("Total Applications: 2")
+  end
 end
