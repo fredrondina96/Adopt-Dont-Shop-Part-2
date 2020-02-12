96bb7cb92aaa2a52c40afc67236250223ab2165e
Cassie Achzenick <caachzenick@gmail.com>
2020-02-11 19:04:03 -0700

    Add user story 30

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,4 +2,27 @@ class Shelter < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   has_many :pets, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+  def total_pets
+    pets.length
+  end
+
+  def average_review_rating
+    score = 0.0
+    total = reviews.length
+    reviews.each do |review|
+      score += review.rating.to_i
+    end
+    score / total
+  end
+
+  def total_applications
+    applications = []
+    pets.each do |pet|
+      pet.applications.each do |application|
+        applications << application.id
+      end
+    end
+    applications.uniq.length
+  end
 end
--- spec/features/shelters/visitor_sees_specific_information_for_specific_shleter_spec.rb
+++ spec/features/shelters/visitor_sees_specific_information_for_specific_shleter_spec.rb
@@ -34,8 +34,8 @@ RSpec.describe "Shelter ID displays all shelter info", type: :feature do
   it "shows statistics for each shelter on its show page" do
     shelter1 = Shelter.create!(name: 'humane society', address: "1234 st", city: 'Denver', state: 'Colorado', zip: "29572")
 
-    review1 = Review.create!(title: "Best Shelter EVER!", rating: "5", content: "I now have my best friend because of this place", shelter_id: shelter1)
-    review2 = Review.create!(title: "Above Average Shelter", rating: "4", content: "I am pretty sure they gave me a dinosaur not a dog.", picture: "https://s7d2.scene7.com/is/image/PetSmart/5290431", shelter_id: shelter1)
+    review1 = Review.create!(title: "Best Shelter EVER!", rating: "5", content: "I now have my best friend because of this place", shelter_id: shelter1.id)
+    review2 = Review.create!(title: "Above Average Shelter", rating: "4", content: "I am pretty sure they gave me a dinosaur not a dog.", picture: "https://s7d2.scene7.com/is/image/PetSmart/5290431", shelter_id: shelter1.id)
 
     snickers = Pet.create!(image: 'https://images-na.ssl-images-amazon.com/images/I/41Q-6cQEOLL._AC_SY400_.jpg', name: 'Snickers', age: 15, sex: 'Female', shelter: shelter1)
     sadie = Pet.create!(image: 'https://images.halloweencostumes.com/products/45834/1-1/dog-dino-pup-costume.jpg', name: 'Sadie', age: 3, sex: "Male", shelter: shelter1)
@@ -50,7 +50,7 @@ RSpec.describe "Shelter ID displays all shelter info", type: :feature do
 
     visit "/shelters/#{shelter1.id}"
 
-    expect(page).to have_content("Total pets: 3")
+    expect(page).to have_content("Total Pets: 3")
     expect(page).to have_content("Average Review Rating: 4.5")
     expect(page).to have_content("Total Applications: 2")
   end
