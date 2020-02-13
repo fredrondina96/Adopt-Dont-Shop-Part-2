dd82d78ae4a2f9b48711811e26431c95b4038ffb
Cassie Achzenick <caachzenick@gmail.com>
2020-02-12 18:00:57 -0700

    Add shelter model tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,9 @@ shelter2 = Shelter.create!(name: 'Meg\'s Shelter', address: "150 Main Street", c
 
 snickers = Pet.create!(image: 'https://images-na.ssl-images-amazon.com/images/I/41Q-6cQEOLL._AC_SY400_.jpg', name: 'Athena', age: 1, sex: 'female', shelter: shelter1, description: 'butthead', adoption_status: "Pending Adoption")
 sadie = Pet.create!(image: 'https://images.halloweencostumes.com/products/45834/1-1/dog-dino-pup-costume.jpg', name: 'Odell', age: 4, sex: "M", shelter: shelter2, description: 'good dog', adoption_status: "Adoptable")
+dog1 = Pet.create!(image: 'https://www.rd.com/wp-content/uploads/2019/09/01_Doggie-dino-760x506.jpg', name: 'Sara', age: 3, sex: "Famale", shelter: shelter2, description: 'Is a Trisaratops', adoption_status: "Adoptable")
+dog2 = Pet.create!(image: 'https://s7d2.scene7.com/is/image/PetSmart/5290431', name: 'Henry', age: 10, sex: "Male", shelter: shelter1, description: 'Has a good life', adoption_status: "Adoptable")
+dog3 = Pet.create!(image: 'https://www.rover.com/blog/wp-content/uploads/2019/09/frisco-raptor-e1567812754551.jpg', name: 'Robert', age: 4, sex: "Female", shelter: shelter1, description: 'Loves the chase', adoption_status: "Adoptable")
+dog3 = Pet.create!(image: 'https://www.rover.com/blog/wp-content/uploads/2019/09/41D19vlgwoL.jpg', name: 'Kathy', age: 2, sex: "Female", shelter: shelter1, description: 'Hates this damn costume', adoption_status: "Adoptable")
+
 review1 = Review.create!(title: "Best Shelter EVER!", rating: "5", content: "I now have my best friend because of this place", shelter_id: shelter1.id)
--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -27,13 +27,27 @@ RSpec.describe Favorite do
     end
   end
 
-  describe "#count_of" do
+  describe "#favorite_count" do
     it "returns the count of all pets in the favorites list" do
       favorite = Favorite.new({})
 
       expect(favorite.favorite_count).to eq(0)
     end
   end
+
+  describe "#pet_check" do
+    it "It checks if a pet is in the shelter" do
+    shelter1 = Shelter.create!(name: 'humane society', address: "1234 st", city: 'Denver', state: 'Colorado', zip: "29572")
+
+    snickers = Pet.create!(image: 'https://images-na.ssl-images-amazon.com/images/I/41Q-6cQEOLL._AC_SY400_.jpg', name: 'Snickers', age: 15, sex: 'Female', shelter: shelter1)
+    sadie = Pet.create!(image: 'https://images.halloweencostumes.com/products/45834/1-1/dog-dino-pup-costume.jpg', name: 'Sadie', age: 3, sex: "Male", shelter: shelter1)
+    abbey = Pet.create!(image: 'https://petcostumecenter.com/wp-content/uploads/2019/08/Pupasaurus-Rex-T-Rex-Dog-Cat-Costume-.png', name: 'Abbey', age: 6, sex: "Male", shelter: shelter1)
+
+    favorite =  Favorite.new({"#{snickers.id}" => 1, "#{sadie.id}" => 1, "#{abbey.id}" => 1})
+
+    expect(favorite.pet_check(snickers.id)).to be_in([true, false])
+  end
+end
 
   describe "#all_pets" do
     it "returns an array of all pet objects that are currently favorited" do
@@ -46,6 +60,20 @@ RSpec.describe Favorite do
       favorite =  Favorite.new({"#{snickers.id}" => 1, "#{sadie.id}" => 1, "#{abbey.id}" => 1})
 
       expect(favorite.all_pets).to eq([snickers, sadie, abbey])
+    end
+  end
+
+  describe "#count_of" do
+    it "counts the number of favorites" do
+      shelter1 = Shelter.create!(name: 'humane society', address: "1234 st", city: 'Denver', state: 'Colorado', zip: "29572")
+
+      snickers = Pet.create!(image: 'https://images-na.ssl-images-amazon.com/images/I/41Q-6cQEOLL._AC_SY400_.jpg', name: 'Snickers', age: 15, sex: 'Female', shelter: shelter1)
+      sadie = Pet.create!(image: 'https://images.halloweencostumes.com/products/45834/1-1/dog-dino-pup-costume.jpg', name: 'Sadie', age: 3, sex: "Male", shelter: shelter1)
+      abbey = Pet.create!(image: 'https://petcostumecenter.com/wp-content/uploads/2019/08/Pupasaurus-Rex-T-Rex-Dog-Cat-Costume-.png', name: 'Abbey', age: 6, sex: "Male", shelter: shelter1)
+
+      favorite =  Favorite.new({"#{snickers.id}" => 1, "#{sadie.id}" => 1, "#{abbey.id}" => 1})
+
+      expect(favorite.count_of(snickers.id)).to eq(1)
     end
   end
 end
